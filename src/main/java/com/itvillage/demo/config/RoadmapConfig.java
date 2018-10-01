9135f5f9089f525240cf043e3c90cff89747eaf8
ITVillage-Kevin <it.village.host@gmail.com>
2018-10-01 13:30:26 +0900

    .

---
+++ src/main/java/com/itvillage/demo/config/RoadmapConfig.java
@@ -0,0 +1,10 @@
+package com.itvillage.demo.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RoadmapConfig {
+
+}

a9f76b6b2d06779a2759578afe93c269c0abefa6
hjs6877 <hjs6877@gmail.com>
2022-04-08 20:23:23 +0900

    Modify TodoController.java

--- src/main/java/com/itvillage/demo/controller/TodoController.java
+++ src/main/java/com/itvillage/demo/controller/TodoController.java
@@ -2,30 +2,26 @@ package com.itvillage.demo.controller;
 
 import com.itvillage.demo.domain.Todo;
 import com.itvillage.demo.repository.TodoRepository;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Slf4j
 @RestController
 public class TodoController {
-    @Autowired
     private TodoRepository todoRepository;
 
-    @RequestMapping(value = "/todo/register", method = RequestMethod.POST)
-    @ResponseBody
-    public Todo register(Todo todo){
+    @Autowired
+    TodoController(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+    @PostMapping(value = "/todo/register")
+    public Todo register(@RequestBody Todo todo){
         todoRepository.save(todo);
         return todo;
     }
 
-    @RequestMapping(value = "/todo/list", method = RequestMethod.GET)
-    @ResponseBody
+    @GetMapping(value = "/todo/list")
     public List<Todo> getTodoList(){
         return todoRepository.findAll();
     }
